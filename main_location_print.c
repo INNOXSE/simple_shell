35876882d8bd022dfb7715f6059f2d6e61569e95
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-05-25 21:30:04 +0100

    Update main_location_print.c

--- main_location_print.c
+++ main_location_print.c
@@ -7,7 +7,17 @@
  */
 int main(void)
 {
-	location_print();
+	list_s *list = NULL;
+	char *location = "PATH";
+	list = locationlist(location, list);
+	list_s *current = list;
 
+	while (current != NULL)
+	{
+		printf("%s\n", current->value);
+		current = current->next;
+	}
+
+	fr_list(list);
 	return (0);
 }
