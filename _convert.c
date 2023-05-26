bbc5a204cfb6f7e87a0155adf144b373c309cbdc
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-05-26 11:13:04 +0100

    Update _convert.c

--- _convert.c
+++ _convert.c
@@ -1,9 +1,10 @@
 #include "shell.h"
+
 /**
- * _convert - searches dirs. in location var. for cmd
- * @cmd: to search for
- * @fulldir: fulldir of command to exe.
- * @location: fulldir var.
+ * _convert - searches directories in the location variable for a command
+ * @command: command to search for
+ * @fulldir: full directory path of the command to execute
+ * @location: location variable
  * Return: pointer to fulldir
  */
 char *_convert(char *command, char *fulldir, char *location)
@@ -13,21 +14,21 @@ char *_convert(char *command, char *fulldir, char *location)
 
 	command_length = _strlen(command);
 	original_location_length = _strlen(location);
-	location_copy = malloc(sizeof(char) * original_location_length + 1);
+	location_copy = malloc(sizeof(char) * (original_location_length + 1));
 	if (location_copy == NULL)
 	{
 		errors(3);
 		return (NULL);
 	}
 	_strcpy(location_copy, location);
-	/* copy location dir. + command name and check if it exists */
+
 	toks = strtok(location_copy, ":");
 	if (toks == NULL)
 		toks = strtok(NULL, ":");
 	while (toks != NULL)
 	{
 		location_length = _strlen(toks);
-		fulldir = malloc(sizeof(char) * (original_location_length + command_length) + 2);
+		fulldir = malloc(sizeof(char) * (original_location_length + command_length + 2));
 		if (fulldir == NULL)
 		{
 			errors(3);
@@ -37,15 +38,13 @@ char *_convert(char *command, char *fulldir, char *location)
 		fulldir[location_length] = '/';
 		_strcpy(fulldir + location_length + 1, command);
 		fulldir[location_length + command_length + 1] = '\0';
-		if (access(fulldir, X_OK) != 0)
-		{
-			free(fulldir);
-			fulldir = NULL;
-			toks = strtok(NULL, ":");
-		}
-		else
+		if (access(fulldir, X_OK) == 0)
 			break;
+		free(fulldir);
+		fulldir = NULL;
+		toks = strtok(NULL, ":");
 	}
 	free(location_copy);
+
 	return (fulldir);
-}
+	}
