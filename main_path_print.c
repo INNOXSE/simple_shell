fa9f4bf14c1e51af3b3d721b773f3075e70bb127
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-05-26 17:50:17 +0100

    Update main_path_print.c

--- main_path_print.c
+++ main_path_print.c
@@ -1,4 +1,7 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * main - uses path_print to print var in path dir
@@ -6,7 +9,32 @@
  */
 int main(void)
 {
-	path_print();
+	path_print()
+{
+    char *path_env = getenv("PATH");  
+    if (path_env == NULL)
+    {
+        printf("PATH environment variable not found\n");
+        return;
+    }
 
-	return (0);
+    char *path_copy = strdup(path_env);  
+    if (path_copy == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return;
+    }
+
+    char *directory = strtok(path_copy, ":");  
+    while (directory != NULL)
+    {
+        printf("%s\n", directory); 
+        directory = strtok(NULL, ":");
+    }
+
+    free(path_copy);  
+}
+    path_print();
+
+    return 0;
 }
