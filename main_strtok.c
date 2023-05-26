c9e95f35ae2db57d52d2eaef01cc73db1b6f6245
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-05-26 18:40:16 +0100

    Update main_strtok.c

--- main_strtok.c
+++ main_strtok.c
@@ -1,46 +1,30 @@
-#include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "holberton.h"
 
 /**
- * path_print - Print directories in the PATH environment variable
- */
-void path_print(void)
-{
-	char *path_env = getenv("PATH");  /* Get the value of the PATH environment variable */
-	if (path_env == NULL)
-	{
-		printf("PATH environment variable not found\n");
-		return;
-	}
-
-	char *path_copy = strdup(path_env);  /* Create a copy of the PATH string */
-	if (path_copy == NULL)
-	{
-		printf("Memory allocation failed\n");
-		return;
-	}
-
-	char *directory = strtok(path_copy, ":");  /* Tokenize the PATH string using ':' delimiter */
-	while (directory != NULL)
-	{
-		printf("%s\n", directory);  /* Print each directory */
-		directory = strtok(NULL, ":");
-	}
-
-	free(path_copy);  /* Free the memory allocated for the copy of the PATH string */
-}
-
-/**
- * main - Entry point of the program
- * @argc: The number of command-line arguments
- * @argv: An array of strings containing the command-line arguments
+ * main - uses strtok to print tokens
+ * @ac: number of arguments
+ * @av: pointer to array of strings
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-	path_print();
+	char *str = NULL;
+	char **tokens = NULL;
+	unsigned int k;
 
-	return 0;
+	str = _getline(stdin, str);
+
+	tokens = _strtok(str, tokens);
+
+	k = 0;
+	while (tokens[k] != NULL)
+	{
+		printf("%s\n", tokens[i]);
+		k++;
+	}
+
+	free(str);
+	free(tokens);
+
+	return (0);
 }
