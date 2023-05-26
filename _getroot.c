84f4647ef3ead0dca71de694b5ef8fb4f30f833d
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-05-26 11:09:19 +0100

    Update _getroot.c

--- _getroot.c
+++ _getroot.c
@@ -2,17 +2,15 @@
 
 /**
  * _getline - puts input from user into buffer root
- * @bf: buffer for user input
+ * @fp: file stream
  * Return: buffer with user input
  */
 char *_getline(FILE *fp)
 {
-	char *line;
+	char *line = NULL;
+	size_t len = 0;
 	ssize_t read;
-	size_t len;
 
-	line = NULL;
-	len = 0;
 	read = getline(&line, &len, fp);
 	if (read == -1)
 	{
