33267bc4b5f9ec0d1ed93feaef883929e1d2a571
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-05-26 11:25:14 +0100

    Update hsh.c

--- hsh.c
+++ hsh.c
@@ -1,39 +1,47 @@
 #include "shell.h"
 
+char **environ; // Definition of environ variable
+
 int main(void)
 {
-	char *line, **toks;
-	struct stat buf;
-	int fp, stat;
+    char *line, **toks;
+    struct stat buf;
+    int fp, stat;
 
-	fp = STDIN_FILENO;
-	prompt(fp, buf);
+    fp = STDIN_FILENO;
+    prompt(fp, buf);
 
-	while ((line = _getline(stdin)) != NULL)
-	{
-		toks = tokenizer(line);
-		if (toks != NULL && toks[0] != NULL)
-		{
-			if (builtin_execute(toks) == 1)
-			{
-				char *command = toks[0];
-				char *location = _getenv("PATH");
-				char *fulldir = NULL;
+    while ((line = _getline(stdin)) != NULL)
+    {
+        toks = tokenizer(line);
+        if (toks != NULL && toks[0] != NULL)
+        {
+            built_t builtin[] = {
+                {"exit", shell_exit},
+                {"env", shell_env},
+                {NULL, NULL}
+            };
 
-				fulldir = _convert(command, fulldir, location);
-				if (fulldir != NULL)
-					child(fulldir, toks, environ);
-				else
-					errors(4);
+            if (builtin_execute(toks, builtin) == 1)
+            {
+                char *command = toks[0];
+                char *location = _getenv("PATH");
+                char *fulldir = NULL;
 
-				free(fulldir);
-			}
-		}
+                fulldir = _convert(command, fulldir, location);
+                if (fulldir != NULL)
+                    child(fulldir, toks, environ);
+                else
+                    errors(4);
 
-		free_all(toks, NULL, line, NULL, 0);
-		prompt(fp, buf);
-	}
+                free(fulldir);
+            }
+        }
 
-	free(line);
-	return (stat);
+        free_all(toks, NULL, line, NULL, 0);
+        prompt(fp, buf);
+    }
+
+    free(line);
+    return stat;
 }
