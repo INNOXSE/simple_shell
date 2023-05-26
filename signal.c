64ee4ce5b6025d7e16b6a5265a9b4d0c18314edc
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-05-26 22:26:26 +0100

    Create signal.c

---
+++ signal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * sigint_handler - handles the SIGINT signal
+ * @signal: the signal number
+ *
+ * Return: void
+ */
+void sigint_handler(int signal)
+{
+    if (signal == SIGINT)
+    {
+        write(STDOUT_FILENO, "\n", 1);
+        prompt();
+        fflush(stdout);
+    }
+}
